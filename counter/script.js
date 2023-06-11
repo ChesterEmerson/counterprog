22d0644c932edc943bde612f3916e7f3176e8795
Chester Olivar <115980680+ChesterEmerson@users.noreply.github.com>
2023-06-11 15:57:47 +0800

    Update script.js

--- counter/script.js
+++ counter/script.js
@@ -4,16 +4,16 @@
  document.getElementById("decreaseBtn").onclick = function (){
      count-=1;
      document.getElementById("countLabel").innerHTML = count;
- 
+     countLabel.style.color = "red";
  }
  document.getElementById("resetBtn").onclick = function (){
      count=0;
      document.getElementById("countLabel").innerHTML = count;
-     
+     countLabel.style.color = "white";
  }
  document.getElementById("increaseBtn").onclick = function (){
      count+=1;
      document.getElementById("countLabel").innerHTML = count;
-     
+     countLabel.style.color = "green";
  } 
  
